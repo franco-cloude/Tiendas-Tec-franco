348c7f1aa000ac0cc2ad074e039689800f9eb4d7
franco-cloude <dumbin.com@gmail.com>
2026-05-03 23:26:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    const nombre = document.getElementById('nombre').value;
-    const email = document.getElementById('email').value;
-    const mensaje = document.getElementById('mensaje').value;
-    const feedback = document.getElementById('formFeedback');
 
-    if(nombre && email && mensaje) {
-        feedback.textContent = `¡Gracias ${nombre}! Tu mensaje ha sido enviado correctamente. Nos contactaremos al correo ${email} pronto.`;
-        feedback.style.color = "green";
-        this.reset(); // Limpia el formulario
-    } else {
-        feedback.textContent = "Por favor, completa todos los campos.";
-        feedback.style.color = "red";
-    }
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Tiendas TEC - Huancayo: Proyecto cargado correctamente.");
+
+    
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
 });
